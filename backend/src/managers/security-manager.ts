6cfeb9edf5d02cbc46358762e8a3296fcc91c814
bluefish7391 <bluefish7391@gmail.com>
2026-04-04 20:11:26 -0400

    feat (security-manager): log status checks

--- backend/src/managers/security-manager.ts
+++ backend/src/managers/security-manager.ts
@@ -9,16 +9,22 @@ export class SecurityManager {
 		if (!googleToken) {
 			throw new BadRequestError("Google token is required.");
 		}
+
 		const decoded = await getAuth().verifyIdToken(googleToken);
 		const email = decoded.email;
+		console.log("getUserTenants: decoded=", decoded);
 		if (!email) {
 			throw new BadRequestError("Email not found in token.");
 		}
+
 		const appUsers = await securityDAO.getAppUsersByEmail(email);
+		console.log("getUserTenants: appUsers=", appUsers);
 		if (!appUsers) {
 			return [];
 		}
+
 		const tenantIDs = new Set(appUsers.map((user) => user.tenantID));
+		console.log("getUserTenants: tenantIDs=", tenantIDs);
 		return await securityDAO.getTenantsByIDs(tenantIDs);
 	}
 
@@ -28,11 +34,13 @@ export class SecurityManager {
 
 		const decoded = await getAuth().verifyIdToken(googleToken);
 		const email = decoded.email;
+		console.log("createAppSession: decoded=", decoded);
 		if (!email) {
 			throw new BadRequestError("Email not found in token.");
 		}
 
 		const appUser = await securityDAO.getAppUserByEmailAndTenantID(email, tenantID);
+		console.log("createAppSession: appUser=", appUser);
 		if (!appUser) {
 			throw new ServerError("User not found for the given tenant.");
 		}
@@ -47,6 +55,7 @@ export class SecurityManager {
 			organizationID: appUser.organizationID,
 			organizationName: organization ? organization.name : "",
 		};
+		console.log("createAppSession: appUserSession=", appUserSession);
 		return appUserSession;
 	}
 }

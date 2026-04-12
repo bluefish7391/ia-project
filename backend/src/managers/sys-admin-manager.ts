e2e9a9d421d3539a67298408ad61b6d744e7955e
bluefish7391 <bluefish7391@gmail.com>
2026-04-12 16:45:23 -0400

    feat(identity): delete user through google identity platform

--- backend/src/managers/sys-admin-manager.ts
+++ backend/src/managers/sys-admin-manager.ts
@@ -1,7 +1,16 @@
+import { getAuth } from "firebase-admin/auth";
 import { RequestContext } from "../request-context";
 
 export class SysAdminManager {
 	async deleteUser(requestContext: RequestContext, email: string): Promise<string | null> {
-		return null; // Placehoder, needs to interact with Google Identity Platform to delete the user.
+		let userRecord;
+		try {
+			userRecord = await getAuth().getUserByEmail(email);
+		} catch {
+			return `No user found with email "${email}".`;
+		}
+
+		await getAuth().deleteUser(userRecord.uid);
+		return null;
 	}
 }
